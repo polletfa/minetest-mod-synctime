e452728e8975393cf342c286e7d4b39fe901e5b2
Fabien Pollet <polletfa@gmail.com>
2017-01-15 17:05:13 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,20 @@
+--[[
+  Synchronize game time with real time every time a player joins.
+  It requires time_speed = 1 to stay synchronized
+]]
+
+print(minetest.get_timeofday())
+minetest.set_timeofday(0.7)
+
+minetest.register_on_joinplayer(function(obj)
+  minetest.log("action", "[MOD] synctime")
+  minetest.log("action", os.date("Current system time: %H:%M:%S"))
+  hour=os.date("%H")
+  min=os.date("%M")
+  sec=os.date("%S")
+  time=(hour+(min+sec/60)/60)/24
+  minetest.log("action", "Current game time: "..minetest.get_timeofday())
+  minetest.log("action", "Set game time to: "..time)
+  minetest.set_timeofday(time)
+  minetest.log("action", "Current game time: "..minetest.get_timeofday())  
+end)
